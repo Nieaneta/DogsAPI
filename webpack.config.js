ea27d2c2352494bc1420574fdb01042f5282268a
Aneta <nieaneta@int.pl>
2019-09-19 13:18:30 +0200

    update loaders

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
                 use: "babel-loader"
             },
             {
-                test: /\.scss$/,
+                test: /\.(scss|sass)$/,
                 use: ["style-loader", "css-loader", "sass-loader"]
             },
             {
